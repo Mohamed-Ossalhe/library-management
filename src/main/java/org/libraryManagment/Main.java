43d097822feb9696a62f423d7d44410c3223dcd3
Mohamed-Ossalhe <ossalhemohamed2@gmail.com>
2023-09-05 14:44:00 +0100

    add main menu

--- src/main/java/org/libraryManagment/Main.java
+++ src/main/java/org/libraryManagment/Main.java
@@ -1,7 +1,23 @@
 package org.libraryManagment;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-
+        System.out.printf("---------------------------------------------%n");
+        System.out.printf("|          Library Management System        |%n");
+        System.out.printf("---------------------------------------------%n");
+        System.out.printf("|                  Main Menu                |%n");
+        System.out.printf("---------------------------------------------%n");
+        System.out.printf("#      1. Main Menu                         #%n");
+        System.out.printf("#      2. Edit Books                        #%n");
+        System.out.printf("#      2. Borrow a Book                     #%n");
+        System.out.printf("#      2. View All Books                    #%n");
+        System.out.printf("#      3. Borrowed Books                    #%n");
+        System.out.printf("#      4. Search for Book                   #%n");
+        System.out.printf("#      4. Statistics                        #%n");
+        System.out.printf("#      5. Exit                              #%n");
+        System.out.printf("---------------------------------------------%n");
+        System.out.printf("# Enter Number >>> ");
     }
 }
