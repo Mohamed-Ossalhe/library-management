12f26d0da4a8d80276e76c02dbbdc2cca23e1400
Mohamed-Ossalhe <ossalhemohamed2@gmail.com>
2023-09-05 14:29:27 +0100

    add database connection using JDBC

--- src/main/java/org/libraryManagment/config/DB.java
+++ src/main/java/org/libraryManagment/config/DB.java
@@ -1,14 +1,38 @@
 package org.libraryManagment.config;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class DB {
-    private final String url = "";
-    private final String dbname = "";
-    private final String username = "";
+    private final String url = "jdbc:mysql://localhost:3306/";
+    private final String dbname = "library_management";
+    private final String username = "root";
     private final String password = "";
     private Connection connection;
 
+    // connection getters & setters
+
+
+    public void setConnection(Connection connection) {
+        this.connection = connection;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
     // constructor
-    public DB() {}
+    public DB() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            setConnection(DriverManager.getConnection(this.url + this.dbname, this.username, this.password));
+        }catch (Exception exception) {
+            System.out.println("Connection Exception: " + exception);
+        }
+    }
+
+    // close connection to db
+    public void closeConnection() throws Exception {
+        getConnection().close();
+    }
 }
